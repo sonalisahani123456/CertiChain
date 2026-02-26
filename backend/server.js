d756a4787bb20c4be124fe682407d0ea49dae8eb
sonalisahani123456 <sonalisahani932@gmail.com>
2026-02-26 07:45:42 +0530

    Updated backend server and package.json

--- backend/server.js
+++ backend/server.js
@@ -4,24 +4,25 @@ const cors = require("cors");
 const certificateRoutes = require("./routes/certificateRoutes");
 require("dotenv").config();
 
-
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/certificates", certificateRoutes);
 
 // Routes
-// const certificateRoutes = require("./routes/certificateRoutes");
-// app.use("/api/certificates", certificateRoutes);
+app.use("/api/certificates", certificateRoutes);
+
+app.get("/", (req, res) => {
+  res.send("CertiChain Backend is Live 🚀");
+});
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch((err) => console.log("MongoDB Error:", err));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
